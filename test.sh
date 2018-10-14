2a691cc230f01f90f8f2211953089146743851d3
root <root@ip-172-31-18-74.cn-north-1.compute.internal>
2018-10-14 08:50:40 +0000

    try sh

--- test.sh
+++ test.sh
@@ -3,7 +3,11 @@
 echo " in test.sh , now test.sh running "
 pwd
 ls
-sh 'printenv'
+sh "printenv"
+sh "mkdir -p output"
+writeFile file: "output/usefulfile.txt", text: "This file is useful, need to archive it."
+
+
 #sudo su -s /bin/bash jenkins
 #whoami
 #/bin/docker image ls -a 
